e23fed57063af6469644b95d45c25db2c004f3de
Xun Xu <120XXu@frhsd.com>
2020-01-15 10:45:47 -0500

    INITIAL COMMIT

---
+++ Index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8080, '0.0.0.0', function () {
+    console.log("\nServer running");
+});
